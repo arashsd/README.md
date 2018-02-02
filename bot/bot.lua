78cdfee59aafeb1d9b7b5447af83f178bf840ec6
arashsd <36054017+arashsd@users.noreply.github.com>
2018-02-02 12:05:31 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -91,3 +91,59 @@
 +    "Tools",
 +    "Write"
 +	},
++    sudo_users = {377450049,418516842,284298227},
++    admins = {},
++    disabled_channels = {},
++    moderation = {data = './data/moderation.json'},
++    info_text = [[
++ 》TiGeR BoT v5.7
++An advanced administration bot based on https://valtman.name/telegram-cli
++
++》https://github.com/BeyondTeam/BDReborn 
++
++》Admins :
++》@Arashsudo ➣ Founder & Developer《
++》@JavadSudo ➣ Developer《
++》@Shaniloop ➣ Developer《
++
++》Special thanks to :
++》TiGeRTeM
++》@Xamarin_Devloper
++
++》Our channel :
++》@TiGeRTem《
++]],
++  }
++ serialize_to_file(config, './data/config.lua')
++ print ('saved config into conf.lua')
++end
++
++-- Returns the config from config.lua file.
++-- If file doesn't exist, create it.
++function load_config( )
++ local f = io.open('./data/config.lua', "r")
++  -- If config.lua doesn't exist
++ if not f then
++  print ("Created new config file: ./data/config.lua")
++  create_config()
++ else
++  f:close()
++ end
++ local config = loadfile ("./data/config.lua")()
++ for v,user in pairs(config.sudo_users) do
++  print("Allowed user: " .. user)
++ end
++ return config
++end
++whoami()
++plugins = {}
++_config = load_config()
++
++function load_plugins()
++ local config = loadfile ("./data/config.lua")()
++ for k, v in pairs(config.enabled_plugins) do
++  print("Loading Plugins", v)
++  local ok, err =  pcall(function()
++  local t = loadfile("plugins/"..v..'.lua')()
++  plugins[v] = t
++  end)
